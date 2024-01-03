f4888f5e1981b90663fc4b1620d08bf80cd33238
Arturo <arturog205@gmail.com>
2024-01-02 23:42:57 -0800

    First Commit: Start Nucamp Website

---
+++ Hearts/i-heart/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Heart from "./components/Heart";
+import Header from "./components/Header";
+import HeartsList from "./components/HeartsList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeartsList />
+    </>
+  );
+}
+
+export default App;
---
+++ nucampsite/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">Hello Nucamp!
+    </div>
+  );
+}
+
+export default App;
